f25e91ddd2cb0da79ed507e902016aba08672094
Crystal Atkinson <crystaltayloratkinson@gmail.com>
2021-01-25 17:22:24 -0500

    add all files

---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+// Stopwatch
+// Create a simple stopwatch app that starts the timer and shows the stopped time.
+
+// Start the stopwatch with a promise
+// End the stopwatch on a random time between 1-10 seconds
+// When the stopwatch stops, display the time it took
+
+
+function stopwatch() {
+    
+}
